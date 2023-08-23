9a90fd058365d4041e269c7fe0e1a8a1281ea5b3
jvbt3 <joaovitormartins077@gmail.com>
2023-08-23 11:57:31 -0300

    Criação de rotas e css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Criar from './components/pages/Create';
+import Contato from './components/pages/Contato';
+import Container from './components/latouys/Container';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/criar">Criar</Link>
+        <Link to="/contato">Contato</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/criar' element={<Criar />} />
+          <Route path='/contato' element={<Contato />} />
+        </Routes>
+      </Container>
+      <footer>footer</footer>
+    </Router>
   );
 }
 
